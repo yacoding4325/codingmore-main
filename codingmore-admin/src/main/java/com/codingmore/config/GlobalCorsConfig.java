6238c233e41b3ec603f7a3a8936f2272aef33e62
yxym0203 <1500081802@qq.com>
2022-07-23 10:26:49 +0800

    跨域配置及learn-admin-security模块相关配置

---
+++ codingmore-admin/src/main/java/com/codingmore/config/GlobalCorsConfig.java
@@ -0,0 +1,38 @@
+package com.codingmore.config;
+
+/**
+ * @Author yaCoding
+ * @create 2022-07-23 上午 10:16
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+/**
+ * 全局跨域配置
+ */
+
+@Configuration
+public class GlobalCorsConfig {
+
+    /**
+     * 允许跨域调用的过滤器
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 允许所有的域名进行跨域调用
+        config.addAllowedOriginPattern("*");
+        //允许跨域发送cookie
+        config.setAllowCredentials(true);
+        //放行全部原始头信息
+        config.addAllowedHeader("*");
+        //允许所有请求方法跨域调用
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}

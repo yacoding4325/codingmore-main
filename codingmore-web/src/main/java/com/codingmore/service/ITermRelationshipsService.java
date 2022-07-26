8b63e718f1fd46421d35ec9aeffafb0a7c51ee18
yxym0203 <1500081802@qq.com>
2022-07-26 17:36:10 +0800

    文章 服务实现类

---
+++ codingmore-web/src/main/java/com/codingmore/service/ITermRelationshipsService.java
@@ -0,0 +1,15 @@
+package com.codingmore.service;
+
+/**
+ * @Author yaCoding
+ * @create 2022-07-26 下午 5:07
+ */
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codingmore.model.TermRelationships;
+
+/**
+ *  * 文章属性关系表 服务类
+ */
+public interface ITermRelationshipsService extends IService<TermRelationships> {
+}
---
+++ codingmore-web/src/main/java/com/codingmore/service/IUsersService.java
@@ -0,0 +1,14 @@
+package com.codingmore.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.codingmore.model.Users;
+
+/**
+ * @Author yaCoding
+ * @create 2022-07-26 下午 4:59
+ */
+/*
+用户表 服务类
+ */
+public interface IUsersService extends IService<Users> {
+}

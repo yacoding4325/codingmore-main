2fafc595ffb1f14c22effd0e8ec4e852dff74de4
yxym0203 <1500081802@qq.com>
2022-09-01 22:37:41 +0800

    后台资源表

---
+++ codingmore-mbg/src/main/java/com/codingmore/dto/ResourceParam.java
@@ -0,0 +1,47 @@
+package com.codingmore.dto;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-01 下午 10:35
+ */
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 后台资源表
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="Resource对象", description="后台资源表")
+public class ResourceParam implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long resourceId;
+
+    @ApiModelProperty(value = "资源分类ID")
+    @NotNull(message="资源分类ID不能为空")
+    private Long categoryId;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "资源名称")
+    @NotBlank(message = "资源名称不能为空")
+    private String name;
+
+    @ApiModelProperty(value = "资源URL")
+    @NotBlank(message = "资源URL不能为空")
+    private String url;
+
+    @ApiModelProperty(value = "描述")
+    private String description;
+}

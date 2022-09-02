8584ff70fe38f4b554dd8c6c0d6c06201dcb3ea4
yxym0203 <1500081802@qq.com>
2022-09-02 23:00:40 +0800

    栏目

--- codingmore-mbg/src/main/java/com/codingmore/dto/TermTaxonomyParam.java
+++ codingmore-mbg/src/main/java/com/codingmore/dto/TermTaxonomyParam.java
@@ -1,9 +1,41 @@
 package com.codingmore.dto;
 
-/**
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+/**栏目
  * @Author yaCoding
  * @create 2022-09-02 下午 10:55
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="TermTaxonomy对象", description="栏目")
+public class TermTaxonomyParam implements Serializable {
 
-public class TermTaxonomyParam {
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "ID")
+
+    private Long termTaxonomyId;
+
+    @ApiModelProperty(value = "说明")
+    private String description;
+
+    @ApiModelProperty(value = "名称")
+    @NotBlank(message="名称不能为空")
+    private String name;
+
+    @ApiModelProperty(value = "父栏目id")
+    private Long parentId;
+
+    @ApiModelProperty("属性")
+    private String attribute;
+
+    private String type;
+
 }

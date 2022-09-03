11240c8bf637d35a5d0be68d8fae45a468253434
yxym0203 <1500081802@qq.com>
2022-09-03 22:49:43 +0800

    用户表

---
+++ codingmore-mbg/src/main/java/com/codingmore/dto/UsersLoginParam.java
@@ -0,0 +1,28 @@
+package com.codingmore.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+/**
+ * @Author yaCoding
+ * @create 2022-09-03 下午 10:48
+ */
+// 用户表
+@Data
+@ApiModel(value="用户登录", description="用户表")
+public class UsersLoginParam implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "登录名")
+    @NotBlank(message="登录名不能为空")
+    private String userLogin;
+
+    @ApiModelProperty(value = "密码")
+    @NotBlank(message="密码不能为空")
+    private String userPass;
+
+}
